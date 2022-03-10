0ad762de70dc471e64933834a84409f246f71d53
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-10 21:19:13 +0300

    HELLO

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,3 @@
+#!/bin/bash
+puts("Programming is like building a multilingual puzzle")\n; return 0
+[A[A[B[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Curn 0
