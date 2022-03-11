d8fe1892a4992915e074fa9ce55b775df3c95810
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-12 02:40:07 +0300

    Coding

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}[A[A       [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cprintf("Enter a number: ");[D[D[D[D[D[C\n");
+        [C[C[C[C[C[C[Cscanf("%d", &n);
+
+[A[B         if (num < 0.0)
+        printf("You entered a negative number.");
+    else if (num > 0.0)
+        printf("You entered a positive number.");
+    else
+        printf("You entered 0.");
+
+    return 0;
+}[A[A[A[A[A[A[A[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C < 0[C[C[C[C[D[D[D[D[C)[C[n is ne[A[D[D[D[D[D[D[>[B[C[C[C[C positive[C[C[D[D."[C[C[C[C[D[D[D[D);[C[C[C[C[C[C[C[C[C[C[[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D < 0 )[C[n is negative.");[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[A[A[D[D[D[D[D[D[D[D[D[ { else [D[D[D[D[D[}[C[C[C[C[C if (n < 0 ){[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D   [D[D } else[B[C[C[C[C[C[C[C[n is 0.\n");[C[C[C[A[A[C[C\n)[A[A[D[D[D[B[C[C[B");[A[A[D[D\n");[B[B[B[B[B[B[B
