2ef026d9a4d942fd6b89cf231a8296be75349e92
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-16 11:58:32 +0300

    print_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*Function to print the alphabets
+ *in lowercase
+ */
+void print_alphabet(void)
+{
+	for (int c =97; c<=122; ++c)
+		_putchar("%c", c);
+	        _putchar("\n");
+
+		return 0;
+}
+
