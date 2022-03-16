1d0b3abc721b9501784bbdd9b9dfac307bcd25a3
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-17 00:52:29 +0300

    MAIN

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,18 +10,18 @@
 
 int main()
 {
-   int a, int b, int c;
+int a, int b, int c;
 
-  if (a >= b && a >= c)
-  {	  
-    printf("a is the largest number.");
-  }
-  else if (b >= a && b >= c)
-{
-    printf(" b is the largest number");
+if (a >= b && a >= c)
+{	  
+printf("a is the largest number.");
 }
-  else
-    printf("c is the largest number");
-    }
-  return 0;
-  }
+else if (b >= a && b >= c)
+{
+printf(" b is the largest number");
+}
+else
+printf("c is the largest number");
+}
+return 0;
+}
