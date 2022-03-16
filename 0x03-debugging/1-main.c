176da3c74dfd83defef711ef4d04843859579dfc
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-16 23:07:45 +0300

    LIKE

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,9 +11,9 @@ int main(void)
         printf("Infinite loop incoming :(\n");
 
         i = 0;
-/**
-*Infinite loop incoming
-*/
+	/**
+	*Infinite loop incoming
+	*/
         while (i < 10)
         {
                 putchar(i);
