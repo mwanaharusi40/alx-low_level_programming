83a033c8154c74d83aa360c5ba8585df87fda5c1
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-17 13:39:11 +0300

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+
+/*
+ * File: 1-isdigit.c
+ * Auth: mwanaaa
+ */
+
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a digit (0-9).
+ * @c: The number to be checked.
+ *
+ * Return: 1 if the number is a digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		
+          _putchar("\n");
+
+}
