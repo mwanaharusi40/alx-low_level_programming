f5a0186d6948f9d066977cad729c9a5f08d1e166
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-17 11:55:16 +0300

    More Functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+/*
+ * File: holberton.h
+ * Auth: Gedeon Obae Gekonge
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,5 +16,9 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-#endif /* MAIN_H */
-~
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
