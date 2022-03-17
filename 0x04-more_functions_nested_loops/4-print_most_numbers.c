0727e5fc10ad473df5665648d7bc7bf9a3805aa9
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-17 09:41:34 +0300

    Most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+    int i;
+
+    for (i=0; i<10; i++);
+
+     if (i == 2 or i==4)
+        
+        _putchar("i");
+        _putchar("\n");
+
+    return (0);
+}
