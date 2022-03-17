3761e1449f4d46ee0793343871c5acc46e45a990
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-17 08:52:33 +0300

    Two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+    int a, int b;
+
+    _putchar("a*b");
+    
+    return (0);
+}
