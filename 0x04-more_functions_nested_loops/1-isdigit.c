4c5fa3f7ca9f8d998c60eb36d060d4b69dc460d9
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-17 08:27:42 +0300

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c);
+{
+    char c;
+
+   if (c = 0)
+    _putchar("0");
+
+   else (c = a)
+    _putchar("a");
+
+    return (0);
+}
