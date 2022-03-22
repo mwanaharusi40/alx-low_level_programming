bf8c81c5c9fe007444e9c8747974a4b6fa0f8ba0
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-22 23:30:36 +0300

    pointers and arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
- * print_rev - func that prints a string, in reverse, followed by a new l line.
- * @s: declaration of s and parameter s for the function as in the quiz
+ * print_rev - func that prints a string, in reverse, followed by a new line
+ * @s: declaration of s and paramters for the function as in the quiz
  * Return: Always 0.
  */
-
 void print_rev(char *s)
 {
 	int c;
@@ -13,7 +11,8 @@ void print_rev(char *s)
 	for (c = 0; s[c] != 0; c++)
 	{
 	}
-	for (c = c - 1; c >= 0; c++)
-		_putchar(s[c])	
+	for (c = c - 1; c >= 0; c--)
+		_putchar(s[c]);
 	_putchar('\n');
+
 }
