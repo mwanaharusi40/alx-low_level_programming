3db806f3c9246f59a039e8f9fd8ce430874918b6
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-22 11:21:54 +0300

    updating value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  int n;
+  int *p;
+
+  n = 98;
+  p = &n;
+  _putchar("n");
+  _putchar("\n");
+
+  return (0);
+}
