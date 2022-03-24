a6d112366fdabdc3812a9198529df8f539c0c629
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-24 23:08:49 +0300

    function that prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,23 +1,24 @@
 /*
  * File: 103-print_buffer.c
-  */
+ */
 
 #include "main.h"
 #include <stdio.h>
 
 /**
- * print_buffer - Prints a buffer 10 bytes at a time
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *                the byte position, then showing the hex content,
+ *                then displaying printable charcaters.
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
  */
 void print_buffer(char *b, int size)
 {
-	nt byte, index;
+	int byte, index;
 
 	for (byte = 0; byte < size; byte += 10)
 	{
-	
-		printf(" byte");
+		printf("%08x: ", byte);
 
 		for (index = 0; index < 10; index++)
 		{
@@ -25,10 +26,31 @@ void print_buffer(char *b, int size)
 				printf("  ");
 
 			else
-				printf( "*(b + index + byte)");
+				printf("%02x", *(b + index + byte));
 
 			if ((index % 2) != 0 && index != 0)
 				printf(" ");
-}
-}
+		}
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+
+			else if (*(b + index + byte) >= 31 &&
+				 *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+
+			else
+				printf(".");
+		}
+
+		if (byte >= size)
+			continue;
+
+		printf("\n");
+	}
+
+	if (size <= 0)
+		printf("\n");
 }
