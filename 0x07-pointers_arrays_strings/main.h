f93ed42c172bb861a2bfb5aad97554c6a041053f
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-28 21:55:04 +0300

    function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ void*_memset(void *s,int c, size_t n);
 char*_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 #endif /* MAIN_H */
