419ed2a70dbb9272a1f10a06c64ab9cdc741aca8
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-28 17:02:17 +0300

    function that copies memory area

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,6 +12,6 @@
 
 int _putchar(char c);
 void*_memset(void *s,int c, size_t n);
-void *_memcpy(void *dest, const void *src, size_t n);
+void*_memcpy(void *dest, const void *src, size_t n);
 
 #endif /* MAIN_H */
