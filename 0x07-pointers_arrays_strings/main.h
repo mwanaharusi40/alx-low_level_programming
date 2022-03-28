62502c6cd037e5c1c03ccf2391533c6fdd12cb9a
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-28 14:53:52 +0300

    function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: Main.h
+ *
+ * Desc: Header file containing prototypes for all functions.
+ * used in the 0x06-pointers_arrays_strings directory.
+ */
+
 #include <stddef.h>
 
 int _putchar(char c);
