8f2002b9eb4f66859af595b9d00fd31925c5c7b3
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-03-31 23:40:20 +0300

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string recursively.
+ * @s: The string to be printed.
+ */
+
+void _puts_recursion(char *s)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
