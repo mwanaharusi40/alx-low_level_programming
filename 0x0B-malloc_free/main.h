069f13793a05c23ce7119bde340f8d01e68f430b
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-04-05 21:19:24 +0300

    a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,11 +5,12 @@
  * File: Main.h
  *
  * Desc: Header file containing prototypes for all functions.
- * used in the 0x06-pointers_arrays_strings directory.
+ * used in the 0x0B-malloc_free.
  */
 
 #include <stdio.h>
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif /* MAIN_H */
