fc54728568b0ddcadc12e8c86162352e352e6feb
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-04-05 20:09:33 +0300

    a function that creates an array of chars, and initializes it with a specific char.

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: Main.h
+ *
+ * Desc: Header file containing prototypes for all functions.
+ * used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stdio.h>
+
+char *create_array(unsigned int size, char c);
+
+#endif /* MAIN_H */
