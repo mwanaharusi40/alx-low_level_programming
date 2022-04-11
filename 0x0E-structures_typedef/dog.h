391f157f93c8283c36aca10137e76618a01cfb21
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-04-11 17:31:41 +0300

    Defining a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+
+/*
+ * File: dog.h
+ * Desc: Defining a new type struct dog with the following elements 
+ *       name, type = char*
+ *       age, type = float
+ *       owner, type = char *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
