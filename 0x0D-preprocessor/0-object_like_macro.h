edd0da245245be3733ced4182daf6d9a44c87f5d
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-04-11 13:53:39 +0300

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,9 @@
 /**
  * File: 0-object_like_macro.h
- * Creating a header file that defines a macro named SIZE as an abbreviation for the token 1024.
+ * Desc: A header file that defines a macro named SIZE as an abbreviation for the token 1024.
  */
 
 #define SIZE 1024
-malloc(SIZE)
+
+
+#endif
