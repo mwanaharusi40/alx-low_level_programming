a197e1cf6fb0dd5da1738b76bab1296fd80c2fea
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-04-14 10:35:24 +0300

    creating prototypes file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/**
+ * File: variadic_functions.h
+ * 
+ * * Desc: Header file containing prototypes for all functions.
+ * used in the 0x10-variadic_functions.
+ */
+
+#include <stddef.h>
+
+int _putchar(char c)
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
