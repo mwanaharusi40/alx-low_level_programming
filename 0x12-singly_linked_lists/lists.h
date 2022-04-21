0e7c7351fea3ca9e39f746151f59232793f2e182
mwanaharusi40 <mwanaharusio18@gmail.com>
2022-04-21 22:19:31 +0300

    updating lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 /*
  * File: lists.h
  * Desc: Header file containing prototypes for all functions
@@ -28,5 +31,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
-
+#endif /* LISTS_H */
